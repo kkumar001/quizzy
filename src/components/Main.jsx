c7e7220d603a40e730a67b87a3cb056b1f0c510d
Kailash Kumar <kailash@kailashs-MacBook-Air.local>
2024-02-19 12:17:32 +0530

    some chngs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Card from './Card'
 
 const Main = () => {
@@ -6,7 +5,7 @@ const Main = () => {
         <div className='lg:w-[calc(100vw-375px)] w-full h-full bg-gradient-to-br flex flex-col gap-[32px]' style={{ backgroundImage: 'linear-gradient(135deg, #303932 0%, #2b2b2b 25%, #2b2b2b 75%, #303932 100%)' }}>
             <div className='h-auto px-10 sm:ml-5 mt-2 sm:text-start text-center'>
                 <h3 className='text-[#fff] text-[42px] font-bold'>Hey, <span className='text-[#f1fe96]'>Name!</span><br />
-                    How's your prep for the <span className='text-[#f1fe96]'>March SAT</span>
+                    How&apos;s your prep for the <span className='text-[#f1fe96]'>March SAT</span>
                 </h3>
             </div>
             <div className='px-10 flex flex-col sm:ml-5'>
